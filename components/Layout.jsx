857fe21845a59ee4f3456edc8d7c815c577cbac4
FredyESPINO <freedyjaeh@gmail.com>
2022-11-18 20:23:48 -0600

    tercer commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
         <Head>
-            <title>Js Mastery Store.</title>
+            <title>Ecomerce Portfolio</title>
         </Head>
         <header>
             <Navbar />
